1e5f03e23e9770d8d3af5c40d132a446d7e6eaab
DimitriBarronmore <d_barronmore@outlook.com>
2023-06-26 13:57:32 -0600

    update with new changes

--- sandbox.lua
+++ sandbox.lua
@@ -111,39 +111,39 @@ end--136
 for key , value in _ENV.pairs ( self . sets ) do --137
 environment [ deepcopy ( key ) ] = deepcopy ( value ) --138
 end--139
-if self . special then --140
-self . special ( environment ) --141
-end--142
-return environment --143
-end--144
-local sandboxes = { } --146
-function sandboxes . clone ( original ) --149
-local new = { } --150
-new . __is_sandbox_template = true --151
-new . keeps = new_set ( ) --152
-new . sets = { } --153
-new . inherits = original . inherits --154
-for key in _ENV.pairs ( original . keeps ) do --155
-new . keeps : add ( key ) --156
-end--157
-for key , value in _ENV.pairs ( original . sets ) do --158
-new . sets [ key ] = value --159
-end--160
-new . special = original . special --161
-new . _G = new --162
+environment . _G = environment --140
+if self . special then --141
+self . special ( environment ) --142
+end--143
+return environment --144
+end--145
+local sandboxes = { } --147
+function sandboxes . clone ( original ) --150
+local new = { } --151
+new . __is_sandbox_template = true --152
+new . keeps = new_set ( ) --153
+new . sets = { } --154
+new . inherits = original . inherits --155
+for key in _ENV.pairs ( original . keeps ) do --156
+new . keeps : add ( key ) --157
+end--158
+for key , value in _ENV.pairs ( original . sets ) do --159
+new . sets [ key ] = value --160
+end--161
+new . special = original . special --162
 _ENV.setmetatable ( new , template_mt ) --163
 return new --164
 end--165
-function sandboxes . new ( inherits , inherit_keeps ) --169
+function sandboxes . new ( parent , inherit_whitelist ) --169
 local new_template = { } --170
 new_template . __is_sandbox_template = true --171
 new_template . keeps = new_set ( ) --172
 new_template . sets = { } --173
-if inherits then --174
-if inherits . __is_sandbox_template then --175
-new_template . inherits = inherits --176
-if inherit_keeps then --177
-for key in _ENV.pairs ( inherits . keeps ) do --178
+if parent then --174
+if parent . __is_sandbox_template then --175
+new_template . inherits = parent --176
+if inherit_whitelist then --177
+for key in _ENV.pairs ( parent . keeps ) do --178
 new_template . keeps : add ( key ) --179
 end--180
 end--181
@@ -151,177 +151,176 @@ else--182
 _ENV.error ( "attempted to inherit from invalid parent" , 2 ) --183
 end--184
 end--185
-new_template . _G = new_template --186
-_ENV.setmetatable ( new_template , template_mt ) --187
-return new_template --188
-end--189
-sandboxes . unsafe = sandboxes . new ( ) --191
-sandboxes . unsafe . sets = initial_environment --192
-sandboxes . restricted = sandboxes . new ( sandboxes . unsafe ) --196
-sandboxes . restricted . keeps : add ( "_VERSION" , "assert" , "error" , "ipairs" , "next" , --197
-"pairs" , "pcall" , "print" , "select" , "tonumber" , "tostring" , "type" , --198
-"warn" , "xpcall" , "unpack" , --199
-"utf8.char" , "utf8.charpattern" , "utf8.codes" , "utf8.codepoint" , --200
-"utf8.len" , "utf8.offset" , --201
-"math.abs" , "math.acos" , "math.asin" , "math.atan" , "math.atan2" , --202
-"math.ceil" , "math.floor" , "math.cos" , "math.cosh" , "math.deg" , --203
-"math.exp" , "math.fmod" , "math.frexp" , "math.huge" , "math.ldexp" , --204
-"math.log" , "math.log10" , "math.max" , "math.min" , "math.modf" , "math.pi" , --205
-"math.pow" , "math.rad" , "math.random" , "math.sin" , "math.sinh" , --206
-"math.sqrt" , "math.tan" , "math.tanh" , --207
-"string.lower" , "string.upper" , "string.find" , --208
-"string.match" , "string.gmatch" , "string.gsub" , "string.format" , --209
-"string.len" , "string.byte" , "string.char" , "string.sub" , --210
-"string.reverse" , --211
-"coroutine.status" , "coroutine.running" , "coroutine.isyieldable" , --212
-"coroutine.create" , "coroutine.yield" , "coroutine.resume" , --213
-"coroutine.wrap" , --214
-"os.clock" , "os.difftime" , "os.time" , --215
-"table.insert" , "table.maxn" , "table.remove" , "table.sort" --216
-)--217
-function sandboxes . _check_file_exists ( filename ) --220
-local file = _ENV.io . open ( filename ) --221
-if file then --222
-file : close ( ) --223
-return true --224
-end--225
-return false --226
-end--227
-sandboxes . _default_require_path = _ENV.package . path --229
-local function filepath_search ( filepath ) --234
-for path in _ENV.package . path : gmatch ( "[^;]+" ) do --235
-local fixed_path = path : gsub ( "%?" , ( filepath : gsub ( "%." , "/" ) ) ) --236
-if sandboxes . _check_file_exists ( fixed_path ) then --243
-return fixed_path --244
+_ENV.setmetatable ( new_template , template_mt ) --186
+return new_template --187
+end--188
+sandboxes . unsafe = sandboxes . new ( ) --190
+sandboxes . unsafe . sets = initial_environment --191
+sandboxes . restricted = sandboxes . new ( sandboxes . unsafe ) --195
+sandboxes . restricted . keeps : add ( "_VERSION" , "assert" , "error" , "ipairs" , "next" , --196
+"pairs" , "pcall" , "print" , "select" , "tonumber" , "tostring" , "type" , --197
+"warn" , "xpcall" , "unpack" , --198
+"utf8.char" , "utf8.charpattern" , "utf8.codes" , "utf8.codepoint" , --199
+"utf8.len" , "utf8.offset" , --200
+"math.abs" , "math.acos" , "math.asin" , "math.atan" , "math.atan2" , --201
+"math.ceil" , "math.floor" , "math.cos" , "math.cosh" , "math.deg" , --202
+"math.exp" , "math.fmod" , "math.frexp" , "math.huge" , "math.ldexp" , --203
+"math.log" , "math.log10" , "math.max" , "math.min" , "math.modf" , "math.pi" , --204
+"math.pow" , "math.rad" , "math.random" , "math.sin" , "math.sinh" , --205
+"math.sqrt" , "math.tan" , "math.tanh" , --206
+"string.lower" , "string.upper" , "string.find" , --207
+"string.match" , "string.gmatch" , "string.gsub" , "string.format" , --208
+"string.len" , "string.byte" , "string.char" , "string.sub" , --209
+"string.reverse" , --210
+"coroutine.status" , "coroutine.running" , "coroutine.isyieldable" , --211
+"coroutine.create" , "coroutine.yield" , "coroutine.resume" , --212
+"coroutine.wrap" , --213
+"os.clock" , "os.difftime" , "os.time" , --214
+"table.insert" , "table.maxn" , "table.remove" , "table.sort" --215
+)--216
+function sandboxes . _check_file_exists ( filename ) --219
+local file = _ENV.io . open ( filename ) --220
+if file then --221
+file : close ( ) --222
+return true --223
+end--224
+return false --225
+end--226
+sandboxes . _default_require_path = _ENV.package . path --228
+local function filepath_search ( filepath ) --233
+for path in _ENV.package . path : gmatch ( "[^;]+" ) do --234
+local fixed_path = path : gsub ( "%?" , ( filepath : gsub ( "%." , "/" ) ) ) --235
+if sandboxes . _check_file_exists ( fixed_path ) then --242
+return fixed_path --243
+end--244
 end--245
 end--246
-end--247
-local function return_lua_searcher ( env ) --249
-local searcher = function ( modulepath ) --250
-local filepath = filepath_search ( modulepath ) --251
-if filepath then --252
-return function ( reqpath ) --253
-local chunk , err = env . loadfile ( filepath , "t" , env ) --254
-if chunk then --255
-return chunk , reqpath --256
-else--257
-_ENV.error ( "error loading module '" .. reqpath .. "'\n" .. ( err or "" ) , 0 ) --258
-end--259
-end --260
-else--261
-local err = ( "\n\tno file '%s.lua' in package.path" ) : format ( modulepath , 2 ) --262
-return err --263
+local function return_lua_searcher ( env ) --248
+local searcher = function ( modulepath ) --249
+local filepath = filepath_search ( modulepath ) --250
+if filepath then --251
+return function ( reqpath ) --252
+local chunk , err = env . loadfile ( filepath , "t" , env ) --253
+if chunk then --254
+return chunk , reqpath --255
+else--256
+_ENV.error ( "error loading module '" .. reqpath .. "'\n" .. ( err or "" ) , 0 ) --257
+end--258
+end --259
+else--260
+local err = ( "\n\tno file '%s.lua' in package.path" ) : format ( modulepath , 2 ) --261
+return err --262
+end--263
 end--264
-end--265
-return searcher --266
-end--267
-local function create_require ( env ) --270
-env . package = env . package or { } --271
-env . package . searchers = { return_lua_searcher ( env ) } --272
-env . package . preload = { } --273
-env . package . loaded = { } --274
-env . package . path = sandboxes . _default_require_path --275
-env . require = function ( modname ) --277
-if env . package . loaded [ modname ] then --278
-return env . package . loaded [ modname ] --279
-end--280
-local loader --282
-local errors = { } --283
-if env . package . preload [ modname ] then --284
-loader = env . package . preload [ modname ] --285
-else--286
-for _ , searcher in _ENV.ipairs ( env . package . searchers ) do --287
-local result = searcher ( modname ) --288
-if _ENV.type ( result ) == "function" then --289
-loader = result --290
-break--291
-else--292
-_ENV.table . insert ( errors , result ) --293
+return searcher --265
+end--266
+local function create_require ( env ) --269
+env . package = env . package or { } --270
+env . package . searchers = { return_lua_searcher ( env ) } --271
+env . package . preload = { } --272
+env . package . loaded = { } --273
+env . package . path = sandboxes . _default_require_path --274
+env . require = function ( modname ) --276
+if env . package . loaded [ modname ] then --277
+return env . package . loaded [ modname ] --278
+end--279
+local loader --281
+local errors = { } --282
+if env . package . preload [ modname ] then --283
+loader = env . package . preload [ modname ] --284
+else--285
+for _ , searcher in _ENV.ipairs ( env . package . searchers ) do --286
+local result = searcher ( modname ) --287
+if _ENV.type ( result ) == "function" then --288
+loader = result --289
+break--290
+else--291
+_ENV.table . insert ( errors , result ) --292
+end--293
 end--294
 end--295
-end--296
-if loader == nil then --297
-_ENV.error ( _ENV.table . concat ( errors ) ) --298
-else--299
-local status , res = _ENV.pcall ( loader , modname ) --300
-if status == false or _ENV.type ( res ) ~= "function" then --301
-_ENV.error ( res , 2 ) --302
-end--303
-local chunk = res --305
-status , res = _ENV.pcall ( chunk , modname ) --306
-if status == false then --307
-_ENV.error ( "error loading module '" .. modname .. "'\n" .. res , 2 ) --308
-end--309
-if res == nil then --310
-res = true --311
-end--312
-env . package . loaded [ modname ] = res --313
-return res --314
+if loader == nil then --296
+_ENV.error ( _ENV.table . concat ( errors ) ) --297
+else--298
+local status , res = _ENV.pcall ( loader , modname ) --299
+if status == false or _ENV.type ( res ) ~= "function" then --300
+_ENV.error ( res , 2 ) --301
+end--302
+local chunk = res --304
+status , res = _ENV.pcall ( chunk , modname ) --305
+if status == false then --306
+_ENV.error ( "error loading module '" .. modname .. "'\n" .. res , 2 ) --307
+end--308
+if res == nil then --309
+res = true --310
+end--311
+env . package . loaded [ modname ] = res --312
+return res --313
+end--314
 end--315
 end--316
-end--317
-local string_metatable = _ENV.getmetatable ( "" ) --319
-sandboxes . protected = sandboxes . clone ( sandboxes . restricted ) --321
-sandboxes . protected . keeps : add ( "setmetatable" , "rawset" , "rawget" , "rawlen" , "rawequal" , "string.dump" , --322
-"string.rep" , "collectgarbage" ) --323
-sandboxes . protected . sets . getmetatable = function ( obj ) --324
-if _ENV.type ( obj ) == "table" then --325
-return _ENV.getmetatable ( obj ) --326
-else--327
-_ENV.error ( "attempt to get dangerous metatable from inside sandbox" , 2 ) --328
+local string_metatable = _ENV.getmetatable ( "" ) --318
+sandboxes . protected = sandboxes . clone ( sandboxes . restricted ) --320
+sandboxes . protected . keeps : add ( "setmetatable" , "rawset" , "rawget" , "rawlen" , "rawequal" , "string.dump" , --321
+"string.rep" , "collectgarbage" ) --322
+sandboxes . protected . sets . getmetatable = function ( obj ) --323
+if _ENV.type ( obj ) == "table" then --324
+return _ENV.getmetatable ( obj ) --325
+else--326
+_ENV.error ( "attempt to get dangerous metatable from inside sandbox" , 2 ) --327
+end--328
 end--329
-end--330
-sandboxes . protected . special = function ( sbox ) --331
-if _ENV._VERSION == "Lua 5.1" then --332
-sbox . load = function ( a , b ) --333
-local chunk , err = _ENV.load ( a , b ) --334
-if chunk then --335
-_ENV.setfenv ( chunk , sbox ) --335
-end --335
-return chunk , err --336
-end--337
-sbox . loadfile = function ( a ) --338
-local chunk , err = _ENV.loadfile ( a ) --339
-if chunk then --340
-_ENV.setfenv ( chunk , sbox ) --340
-end --340
-return chunk , err --341
-end--342
-sbox . loadstring = function ( a ) --343
-local chunk , err = _ENV.loadstring ( a ) --344
-if chunk then --345
-_ENV.setfenv ( chunk , sbox ) --345
-end --345
-return chunk , err --346
-end--347
-else--348
-sbox . load = function ( a , b , c , env ) --349
-if env == nil then --350
-env = sbox --351
-end--352
-return _ENV.load ( a , b , c , env ) --353
-end--354
-sbox . loadfile = function ( a , b , env ) --355
-if env == nil then --356
-env = sbox --357
-end--358
-return _ENV.loadfile ( a , b , env ) --359
-end--360
-sbox . loadstring = function ( string , env ) --361
-if env == nil then --362
-env = sbox --363
-end--364
-return _ENV.load ( string , nil , "t" , env ) --365
+sandboxes . protected . special = function ( sbox ) --330
+if _ENV._VERSION == "Lua 5.1" then --331
+sbox . load = function ( a , b ) --332
+local chunk , err = _ENV.load ( a , b ) --333
+if chunk then --334
+_ENV.setfenv ( chunk , sbox ) --334
+end --334
+return chunk , err --335
+end--336
+sbox . loadfile = function ( a ) --337
+local chunk , err = _ENV.loadfile ( a ) --338
+if chunk then --339
+_ENV.setfenv ( chunk , sbox ) --339
+end --339
+return chunk , err --340
+end--341
+sbox . loadstring = function ( a ) --342
+local chunk , err = _ENV.loadstring ( a ) --343
+if chunk then --344
+_ENV.setfenv ( chunk , sbox ) --344
+end --344
+return chunk , err --345
+end--346
+else--347
+sbox . load = function ( a , b , c , env ) --348
+if env == nil then --349
+env = sbox --350
+end--351
+return _ENV.load ( a , b , c , env ) --352
+end--353
+sbox . loadfile = function ( a , b , env ) --354
+if env == nil then --355
+env = sbox --356
+end--357
+return _ENV.loadfile ( a , b , env ) --358
+end--359
+sbox . loadstring = function ( string , env ) --360
+if env == nil then --361
+env = sbox --362
+end--363
+return _ENV.load ( string , nil , "t" , env ) --364
+end--365
 end--366
-end--367
-sbox . dofile = function ( a ) --370
-local chunk , err = sbox . loadfile ( a ) --371
-if err then --372
-_ENV.error ( err , 2 ) --372
-end --372
-return chunk ( ) --373
-end--374
-create_require ( sbox ) --375
-end--376
-return sandboxes --378
+sbox . dofile = function ( a ) --369
+local chunk , err = sbox . loadfile ( a ) --370
+if err then --371
+_ENV.error ( err , 2 ) --371
+end --371
+return chunk ( ) --372
+end--373
+create_require ( sbox ) --374
+end--375
+return sandboxes --377
 ---
